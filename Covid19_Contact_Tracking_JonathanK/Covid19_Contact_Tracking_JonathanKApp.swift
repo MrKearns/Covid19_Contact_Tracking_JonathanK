07172a5b6ac8f6560d87b5a629062998e494ab12
MrKearns <jkearns631@students.mchenry.edu>
2023-02-02 09:48:16 -0600

    Initial Commit

---
+++ Covid19_Contact_Tracking_JonathanK/Covid19_Contact_Tracking_JonathanKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19_Contact_Tracking_JonathanKApp.swift
+//  Covid19_Contact_Tracking_JonathanK
+//
+//  Created by Jonathan Kearns on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Covid19_Contact_Tracking_JonathanKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
